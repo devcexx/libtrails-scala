0423655330b093610d258dbb45d4e1baef8f5d8d
Roberto Guillén <robertoguillen@me.com>
2017-06-08 21:29:00 +0200

    Progress

--- src/main/scala/com/devcexx/libtrails/scala/Math.scala
+++ src/main/scala/com/devcexx/libtrails/scala/Math.scala
@@ -17,58 +17,120 @@ package com.devcexx.libtrails.scala
 import scala.{specialized=>spec}
 
 object linearTransf {
+
+  def translate[A <: Vector[A]](offset: A)(vector: A): A = offset
+
   def rotate2[A](angle: Double): FloatingVector2[A] => FloatingVector2[A] = {
     val cos = math.cos(angle)
     val sin = math.sin(angle)
 
     x =>
       val n = x.n
-      val cos1 = x.conv(cos)
-      val sin1 = x.conv(sin)
+      val cos1 = x.conv.fromDouble(cos)
+      val sin1 = x.conv.fromDouble(sin)
       x.builder(
         n.minus(n.times(x.x, cos1), n.times(x.y, sin1)),
         n.plus(n.times(x.x, sin1), n.times(x.y, cos1))
       )
   }
+
   def scale2[A](factor: A): FloatingVector2[A] => FloatingVector2[A] =
-    x => x.normalize(x.n.times(x.conv(x.norm()), factor))
+    x => x.normalize(x.n.times(x.conv.fromDouble(x.norm()), factor))
+
+  def rotate3[A](axis: FloatingVector3[A], angle: Double): FloatingVector3[A] => FloatingVector3[A] = {
+    val cos = math.cos(angle)
+    val sin = math.sin(angle)
+    val k = axis.normalize()
+
+    v => {
+      val n = v.n
+      val cos1 = v.conv.fromDouble(cos)
+      val sin1 = v.conv.fromDouble(sin)
+      val cross = k × v
+      val dot = n.times(k dot v, n.minus(n.one, cos1))
+      v.builder(
+        n.plus(n.plus(n.times(v.x, cos1), n.times(cross.x, sin1)), n.times(k.x, dot)),
+        n.plus(n.plus(n.times(v.y, cos1), n.times(cross.y, sin1)), n.times(k.y, dot)),
+        n.plus(n.plus(n.times(v.z, cos1), n.times(cross.z, sin1)), n.times(k.z, dot))
+      )
+    }
+  }
+
+
 }
 
-trait Vector[@spec(Int, Double, Float) A] extends Traversable[A] {
-  type Vec <: Vector[A]
+object Vectors {
+  type Vector2i = IntegralVector2[Int]
+  type Vector2l = IntegralVector2[Long]
+  type Vector2f = FloatingVector2[Float]
+  type Vector2d = FloatingVector2[Double]
+
+  type Vector3i = IntegralVector3[Int]
+  type Vector3l = IntegralVector3[Long]
+  type Vector3f = FloatingVector3[Float]
+  type Vector3d = FloatingVector3[Double]
+
+  object IntegralVector2 {
+    def apply[A](x: A, y: A)(implicit n: Integral[A], conv: NumberConverter[A])
+    = new IntegralVector2(x, y)(n, conv)
+  }
+
+  object FloatingVector2 {
+    def apply[A](x: A, y: A)(implicit n: Fractional[A], conv: NumberConverter[A])
+    = new FloatingVector2(x, y)(n, conv)
+  }
+
+  object IntegralVector3 {
+    def apply[A](x: A, y: A, z: A)(implicit n: Integral[A], conv: NumberConverter[A])
+    = new IntegralVector3(x, y, z)(n, conv)
+  }
+
+  object FloatingVector3 {
+    def apply[A](x: A, y: A, z: A)(implicit n: Fractional[A], conv: NumberConverter[A])
+    = new FloatingVector3(x, y, z)(n, conv)
+  }
+}
+
+trait Vector[@spec(Int, Long, Double, Float) A] extends Traversable[A] {
+  type VecGen <: Vector[A]
+  type VecSelf <: VecGen
+
   val n: Numeric[A]
+  val conv: NumberConverter[A]
 
-  def add(o: Vec): Vec
-  def sub(o: Vec): Vec
-  def mul(k: A): Vec
-  def neg(): Vec
+  def add(o: VecGen): VecSelf
+  def sub(o: VecGen): VecSelf
+  def mul(k: A): VecSelf
+  def neg(): VecSelf
 
-  def dot(o: Vec): A
-  def normSq(): A = this.dot(this.asInstanceOf[Vec])
+  def dot(o: VecGen): A
+  def normSq(): A
   def norm() = math.sqrt(n.toDouble(normSq()))
 
-  def distanceSq(o: Vec): A = (this - o).normSq()
-  def cosine(o: Vec) = n.toDouble(dot(o)) / (norm() * o.norm())
-  def angle(o: Vec) = math.acos(cosine(o))
+  def distanceSq(o: VecGen): A = (this - o).normSq()
+  def cosine(o: VecGen) = n.toDouble(dot(o)) / (norm() * o.norm())
+  def angle(o: VecGen) = math.acos(cosine(o))
 
-  def +(o: Vec): Vec = add(o)
-  def *(k: A): Vec = mul(k)
-  def unary_-(): Vec = neg()
-  def -(o: Vec): Vec = sub(o)
+  def +(o: VecGen): VecSelf = add(o)
+  def *(k: A): VecSelf = mul(k)
+  def unary_-(): VecSelf = neg()
+  def -(o: VecGen): VecSelf = sub(o)
 }
 
 trait Vector2[A] extends Vector[A] {
-  override type Vec <: Vector2[A]
-  val builder: (A, A) => Vec
+  final override type VecGen = Vector2[A]
+  override type VecSelf <: Vector2[A]
+  val builder: (A, A) => VecSelf
 
   val x: A
   val y: A
 
-  override def add(o: Vec) = builder(n.plus(x, o.x), n.plus(y, o.y))
-  override def sub(o: Vec) = builder(n.minus(x, o.x), n.minus(y, o.y))
+  override def add(o: Vector2[A]) = builder(n.plus(x, o.x), n.plus(y, o.y))
+  override def sub(o: Vector2[A]) = builder(n.minus(x, o.x), n.minus(y, o.y))
   override def mul(k: A) = builder(n.times(x, k), n.times(y, k))
   override def neg() = builder(n.negate(x), n.negate(y))
-  override def dot(o: Vec): A = n.plus(n.times(this.x, o.x),
+  override def normSq(): A = dot(this)
+  override def dot(o: Vector2[A]): A = n.plus(n.times(this.x, o.x),
     n.times(this.y, o.y))
 
   override def foreach[U](f: (A) => U): Unit = {
@@ -78,88 +140,107 @@ trait Vector2[A] extends Vector[A] {
 }
 
 trait IntegralVector[A] extends Vector[A] {
-  override type Vec <: IntegralVector[A]
-  val n: Integral[A]
+  override val n: Integral[A]
 }
 
 trait FloatingVector[A] extends Vector[A] {
-  override type Vec <: FloatingVector[A]
-  val n: Fractional[A]
+  override val n: Fractional[A]
 
-  def normalize(mod: A): Vec = normalize().mul(mod).asInstanceOf[Vec]
-  def normalize(): Vec
+  def normalize(mod: A): VecSelf = normalize().mul(mod).asInstanceOf[VecSelf]
+  def normalize(): VecSelf
 }
 
 final class IntegralVector2[A](override val x: A, override val y: A)
-                              (implicit numeric: Integral[A])
+                              (implicit numeric: Integral[A],
+                               override val conv: NumberConverter[A])
   extends Vector2[A] with IntegralVector[A] {
 
-  override type Vec = IntegralVector2[A]
-  override val builder = new IntegralVector2(_, _)(numeric)
+  override type VecSelf = IntegralVector2[A]
+  override val builder = new IntegralVector2(_, _)(numeric, conv)
   override val n: Integral[A] = numeric
 }
 
 final class FloatingVector2[A](override val x: A, override val y: A)
                               (implicit numeric: Fractional[A],
-                           val conv: NumberConverter[Double, A])
+                           val conv: NumberConverter[A])
   extends Vector2[A] with FloatingVector[A] {
 
-  override type Vec = FloatingVector2[A]
+  override type VecSelf = FloatingVector2[A]
   override val n: Fractional[A] = numeric
   override val builder = new FloatingVector2(_, _)(numeric, conv)
 
   override def normalize(): FloatingVector2[A] = {
-    val mod = conv(norm())
+    val mod = conv.fromDouble(norm())
     builder(n.div(x, mod), n.div(y, mod))
   }
 }
 
 trait Vector3[A] extends Vector[A] {
+  final override type VecGen = Vector3[A]
+  override type VecSelf <: Vector3[A]
+  val builder: (A, A, A) => VecSelf
 
   val x: A
   val y: A
   val z: A
 
-  val builder: (A, A, A) => Vec
-
-  override type Vec <: Vector3[A]
-  override def add(o: Vec): Vec = builder(n.plus(x, o.x),
+  override def add(o: Vector3[A]): VecSelf = builder(n.plus(x, o.x),
     n.plus(y, o.y), n.plus(z, o.z))
-  override def sub(o: Vec): Vec = builder(n.minus(x, o.x),
+  override def sub(o: Vector3[A]): VecSelf = builder(n.minus(x, o.x),
     n.minus(y, o.y), n.minus(z, o.z))
-  override def mul(k: A): Vec = builder(n.times(k, x),
+  override def mul(k: A): VecSelf = builder(n.times(k, x),
     n.times(k, y), n.times(k, z))
-  override def neg(): Vec = builder(n.negate(x),
+  override def neg(): VecSelf = builder(n.negate(x),
     n.negate(y), n.negate(z))
-  override def dot(o: Vec): A = n.plus(n.plus(n.times(this.x, o.x),
+  override def dot(o: Vector3[A]): A = n.plus(n.plus(n.times(this.x, o.x),
     n.times(this.y, o.y)), n.times(this.z, o.z))
+  override def normSq(): A = dot(this)
+
+  def mixed(cross1: Vector3[A], cross2: Vector3[A]): A = cross1.cross(cross2).dot(this)
+
+  def cross(o: Vector3[A]): VecSelf = builder(
+    n.minus(n.times(y, o.z), n.times(o.y, z)),
+    n.minus(n.times(z, o.x), n.times(o.z, x)),
+    n.minus(n.times(x, o.y), n.times(o.x, y))
+  )
+
+  def fullAngle(o: VecGen, normal: VecGen) = {
+    val dot = this.dot(o)
+    val det = normal.mixed(this.asInstanceOf[VecGen], o)
+    val angle = math.atan2(conv.toDouble(det), conv.toDouble(dot))
+    if (angle > 0) angle
+    else angle + 2 * Math.PI
+  }
+
   override def foreach[U](f: (A) => U): Unit = {
     f(x)
     f(y)
     f(z)
   }
+
+  def ×(o: Vector3[A]) = cross(o)
 }
 
 final class IntegralVector3[A](override val x: A, override val y: A,
                                override val z: A)
                               (implicit numeric: Integral[A],
-                               val conv: NumberConverter[Double, A])
+                               override val conv: NumberConverter[A])
   extends Vector3[A] with IntegralVector[A] {
 
   override val builder: (A, A, A) => IntegralVector3[A] =
     new IntegralVector3(_, _, _)(numeric, conv)
 
-  override type Vec = IntegralVector3[A]
+  override type VecSelf = IntegralVector3[A]
   override val n: Integral[A] = numeric
 }
 
 final class FloatingVector3[A](override val x: A, override val y: A,
                                override val z: A)
                               (implicit numeric: Fractional[A],
-                               val conv: NumberConverter[Double, A])
+                               override val conv: NumberConverter[A])
   extends Vector3[A] with FloatingVector[A] {
 
-  override type Vec = FloatingVector3[A]
+  override type VecSelf = FloatingVector3[A]
   override val builder: (A, A, A) => FloatingVector3[A] =
     new FloatingVector3(_, _, _)(numeric, conv)
 
@@ -167,7 +248,7 @@ final class FloatingVector3[A](override val x: A, override val y: A,
 
 
   override def normalize(): FloatingVector3[A] = {
-    val mod = conv(norm())
+    val mod = conv.fromDouble(norm())
     builder(n.div(x, mod), n.div(y, mod), n.div(z,mod))
   }
 }
--- src/main/scala/com/devcexx/libtrails/scala/NumConversions.scala
+++ src/main/scala/com/devcexx/libtrails/scala/NumConversions.scala
@@ -15,426 +15,56 @@
  */
 package com.devcexx.libtrails.scala
 
-trait NumberConverter[A,B] {
-  def apply(o: A): B
-  def reverse()(implicit conv: NumberConverter[B, A]) = conv
-
+trait NumberConverter[A] {
+  def fromInt(n: Int): A
+  def fromLong(n: Long): A
+  def fromFloat(n: Float): A
+  def fromDouble(n: Double): A
+  def toInt(n: A): Int
+  def toLong(n: A): Long
+  def toFloat(n: A): Float
+  def toDouble(n: A): Double
 }
 
 object NumberConverters {
-  //From Byte
-  implicit object ByteToByteConverter 
-    extends NumberConverter[Byte, Byte] {
-
-    override def apply(o: Byte): Byte = o
-  }
-  implicit object ByteToShortConverter 
-    extends NumberConverter[Byte, Short] {
-
-    override def apply(o: Byte): Short = o.toShort
-  }
-  implicit object ByteToCharConverter 
-    extends NumberConverter[Byte, Char] {
-
-    override def apply(o: Byte): Char = o.toChar
-  }
-  implicit object ByteToIntConverter 
-    extends NumberConverter[Byte, Int] {
-
-    override def apply(o: Byte): Int = o.toInt
-  }
-  implicit object ByteToLongConverter 
-    extends NumberConverter[Byte, Long] {
-
-    override def apply(o: Byte): Long = o.toLong
-  }
-  implicit object ByteToFloatConverter 
-    extends NumberConverter[Byte, Float] {
-
-    override def apply(o: Byte): Float = o.toFloat
-  }
-  implicit object ByteToDoubleConverter 
-    extends NumberConverter[Byte, Double] {
-
-    override def apply(o: Byte): Double = o.toDouble
-  }
-  implicit object ByteToBigIntConverter 
-    extends NumberConverter[Byte, BigInt] {
-
-    override def apply(o: Byte): BigInt = BigInt(o)
-  }
-  implicit object ByteToBigDecimalConverter 
-    extends NumberConverter[Byte, BigDecimal] {
-
-    override def apply(o: Byte): BigDecimal = BigDecimal(o)
-  }
-  //From Short
-  implicit object ShortToByteConverter 
-    extends NumberConverter[Short, Byte] {
-
-    override def apply(o: Short): Byte = o.toByte
-  }
-  implicit object ShortToShortConverter 
-    extends NumberConverter[Short, Short] {
-
-    override def apply(o: Short): Short = o
-  }
-  implicit object ShortToCharConverter 
-    extends NumberConverter[Short, Char] {
-
-    override def apply(o: Short): Char = o.toChar
-  }
-  implicit object ShortToIntConverter 
-    extends NumberConverter[Short, Int] {
-
-    override def apply(o: Short): Int = o.toInt
-  }
-  implicit object ShortToLongConverter 
-    extends NumberConverter[Short, Long] {
-
-    override def apply(o: Short): Long = o.toLong
-  }
-  implicit object ShortToFloatConverter 
-    extends NumberConverter[Short, Float] {
-
-    override def apply(o: Short): Float = o.toFloat
-  }
-  implicit object ShortToDoubleConverter 
-    extends NumberConverter[Short, Double] {
-
-    override def apply(o: Short): Double = o.toDouble
-  }
-  implicit object ShortToBigIntConverter 
-    extends NumberConverter[Short, BigInt] {
-
-    override def apply(o: Short): BigInt = BigInt(o)
-  }
-  implicit object ShortToBigDecimalConverter 
-    extends NumberConverter[Short, BigDecimal] {
-
-    override def apply(o: Short): BigDecimal = BigDecimal(o)
-  }
-  //From Char
-  implicit object CharToByteConverter 
-    extends NumberConverter[Char, Byte] {
-
-    override def apply(o: Char): Byte = o.toByte
-  }
-  implicit object CharToShortConverter 
-    extends NumberConverter[Char, Short] {
-
-    override def apply(o: Char): Short = o.toShort
-  }
-  implicit object CharToCharConverter 
-    extends NumberConverter[Char, Char] {
-
-    override def apply(o: Char): Char = o
-  }
-  implicit object CharToIntConverter 
-    extends NumberConverter[Char, Int] {
-
-    override def apply(o: Char): Int = o.toInt
-  }
-  implicit object CharToLongConverter 
-    extends NumberConverter[Char, Long] {
-
-    override def apply(o: Char): Long = o.toLong
-  }
-  implicit object CharToFloatConverter 
-    extends NumberConverter[Char, Float] {
-
-    override def apply(o: Char): Float = o.toFloat
-  }
-  implicit object CharToDoubleConverter 
-    extends NumberConverter[Char, Double] {
-
-    override def apply(o: Char): Double = o.toDouble
-  }
-  implicit object CharToBigIntConverter 
-    extends NumberConverter[Char, BigInt] {
-
-    override def apply(o: Char): BigInt = BigInt(o)
-  }
-  implicit object CharToBigDecimalConverter 
-    extends NumberConverter[Char, BigDecimal] {
-
-    override def apply(o: Char): BigDecimal = BigDecimal(o)
-  }
-  //From Int
-  implicit object IntToByteConverter 
-    extends NumberConverter[Int, Byte] {
-
-    override def apply(o: Int): Byte = o.toByte
-  }
-  implicit object IntToShortConverter 
-    extends NumberConverter[Int, Short] {
-
-    override def apply(o: Int): Short = o.toShort
-  }
-  implicit object IntToCharConverter 
-    extends NumberConverter[Int, Char] {
-
-    override def apply(o: Int): Char = o.toChar
-  }
-  implicit object IntToIntConverter 
-    extends NumberConverter[Int, Int] {
-
-    override def apply(o: Int): Int = o
-  }
-  implicit object IntToLongConverter 
-    extends NumberConverter[Int, Long] {
-
-    override def apply(o: Int): Long = o.toLong
-  }
-  implicit object IntToFloatConverter 
-    extends NumberConverter[Int, Float] {
-
-    override def apply(o: Int): Float = o.toFloat
-  }
-  implicit object IntToDoubleConverter 
-    extends NumberConverter[Int, Double] {
-
-    override def apply(o: Int): Double = o.toDouble
-  }
-  implicit object IntToBigIntConverter 
-    extends NumberConverter[Int, BigInt] {
-
-    override def apply(o: Int): BigInt = BigInt(o)
-  }
-  implicit object IntToBigDecimalConverter 
-    extends NumberConverter[Int, BigDecimal] {
-
-    override def apply(o: Int): BigDecimal = BigDecimal(o)
-  }
-  //From Long
-  implicit object LongToByteConverter 
-    extends NumberConverter[Long, Byte] {
-
-    override def apply(o: Long): Byte = o.toByte
-  }
-  implicit object LongToShortConverter 
-    extends NumberConverter[Long, Short] {
-
-    override def apply(o: Long): Short = o.toShort
-  }
-  implicit object LongToCharConverter 
-    extends NumberConverter[Long, Char] {
-
-    override def apply(o: Long): Char = o.toChar
-  }
-  implicit object LongToIntConverter 
-    extends NumberConverter[Long, Int] {
-
-    override def apply(o: Long): Int = o.toInt
-  }
-  implicit object LongToLongConverter 
-    extends NumberConverter[Long, Long] {
-
-    override def apply(o: Long): Long = o
-  }
-  implicit object LongToFloatConverter 
-    extends NumberConverter[Long, Float] {
-
-    override def apply(o: Long): Float = o.toFloat
-  }
-  implicit object LongToDoubleConverter 
-    extends NumberConverter[Long, Double] {
-
-    override def apply(o: Long): Double = o.toDouble
-  }
-  implicit object LongToBigIntConverter 
-    extends NumberConverter[Long, BigInt] {
-
-    override def apply(o: Long): BigInt = BigInt(o)
-  }
-  implicit object LongToBigDecimalConverter 
-    extends NumberConverter[Long, BigDecimal] {
-
-    override def apply(o: Long): BigDecimal = BigDecimal(o)
-  }
-  //From Float
-  implicit object FloatToByteConverter 
-    extends NumberConverter[Float, Byte] {
-
-    override def apply(o: Float): Byte = o.toByte
-  }
-  implicit object FloatToShortConverter 
-    extends NumberConverter[Float, Short] {
-
-    override def apply(o: Float): Short = o.toShort
-  }
-  implicit object FloatToCharConverter 
-    extends NumberConverter[Float, Char] {
-
-    override def apply(o: Float): Char = o.toChar
-  }
-  implicit object FloatToIntConverter 
-    extends NumberConverter[Float, Int] {
-
-    override def apply(o: Float): Int = o.toInt
-  }
-  implicit object FloatToLongConverter 
-    extends NumberConverter[Float, Long] {
-
-    override def apply(o: Float): Long = o.toLong
-  }
-  implicit object FloatToFloatConverter 
-    extends NumberConverter[Float, Float] {
-
-    override def apply(o: Float): Float = o
-  }
-  implicit object FloatToDoubleConverter 
-    extends NumberConverter[Float, Double] {
-
-    override def apply(o: Float): Double = o.toDouble
-  }
-  implicit object FloatToBigIntConverter 
-    extends NumberConverter[Float, BigInt] {
-
-    override def apply(o: Float): BigInt = BigInt(o.toLong)
-  }
-  implicit object FloatToBigDecimalConverter 
-    extends NumberConverter[Float, BigDecimal] {
-
-    override def apply(o: Float): BigDecimal = BigDecimal(o)
-  }
-  //From Double
-  implicit object DoubleToByteConverter 
-    extends NumberConverter[Double, Byte] {
-
-    override def apply(o: Double): Byte = o.toByte
-  }
-  implicit object DoubleToShortConverter 
-    extends NumberConverter[Double, Short] {
-
-    override def apply(o: Double): Short = o.toShort
-  }
-  implicit object DoubleToCharConverter 
-    extends NumberConverter[Double, Char] {
-
-    override def apply(o: Double): Char = o.toChar
-  }
-  implicit object DoubleToIntConverter 
-    extends NumberConverter[Double, Int] {
-
-    override def apply(o: Double): Int = o.toInt
-  }
-  implicit object DoubleToLongConverter 
-    extends NumberConverter[Double, Long] {
-
-    override def apply(o: Double): Long = o.toLong
-  }
-  implicit object DoubleToFloatConverter 
-    extends NumberConverter[Double, Float] {
-
-    override def apply(o: Double): Float = o.toFloat
-  }
-  implicit object DoubleToDoubleConverter 
-    extends NumberConverter[Double, Double] {
-
-    override def apply(o: Double): Double = o
-  }
-  implicit object DoubleToBigIntConverter 
-    extends NumberConverter[Double, BigInt] {
-
-    override def apply(o: Double): BigInt = BigInt(o.toLong)
-  }
-  implicit object DoubleToBigDecimalConverter 
-    extends NumberConverter[Double, BigDecimal] {
-
-    override def apply(o: Double): BigDecimal = BigDecimal(o)
-  }
-  //From BigInt
-  implicit object BigIntToByteConverter 
-    extends NumberConverter[BigInt, Byte] {
-
-    override def apply(o: BigInt): Byte = o.toByte
-  }
-  implicit object BigIntToShortConverter 
-    extends NumberConverter[BigInt, Short] {
-
-    override def apply(o: BigInt): Short = o.toShort
-  }
-  implicit object BigIntToCharConverter 
-    extends NumberConverter[BigInt, Char] {
-
-    override def apply(o: BigInt): Char = o.toChar
-  }
-  implicit object BigIntToIntConverter 
-    extends NumberConverter[BigInt, Int] {
-
-    override def apply(o: BigInt): Int = o.toInt
-  }
-  implicit object BigIntToLongConverter 
-    extends NumberConverter[BigInt, Long] {
-
-    override def apply(o: BigInt): Long = o.toLong
-  }
-  implicit object BigIntToFloatConverter 
-    extends NumberConverter[BigInt, Float] {
-
-    override def apply(o: BigInt): Float = o.toFloat
-  }
-  implicit object BigIntToDoubleConverter 
-    extends NumberConverter[BigInt, Double] {
-
-    override def apply(o: BigInt): Double = o.toDouble
-  }
-  implicit object BigIntToBigIntConverter 
-    extends NumberConverter[BigInt, BigInt] {
-
-    override def apply(o: BigInt): BigInt = o
-  }
-  implicit object BigIntToBigDecimalConverter 
-    extends NumberConverter[BigInt, BigDecimal] {
-
-    override def apply(o: BigInt): BigDecimal = BigDecimal(o)
-  }
-  //From BigDecimal
-  implicit object BigDecimalToByteConverter 
-    extends NumberConverter[BigDecimal, Byte] {
-
-    override def apply(o: BigDecimal): Byte = o.toByte
-  }
-  implicit object BigDecimalToShortConverter 
-    extends NumberConverter[BigDecimal, Short] {
-
-    override def apply(o: BigDecimal): Short = o.toShort
-  }
-  implicit object BigDecimalToCharConverter 
-    extends NumberConverter[BigDecimal, Char] {
-
-    override def apply(o: BigDecimal): Char = o.toChar
-  }
-  implicit object BigDecimalToIntConverter 
-    extends NumberConverter[BigDecimal, Int] {
-
-    override def apply(o: BigDecimal): Int = o.toInt
-  }
-  implicit object BigDecimalToLongConverter 
-    extends NumberConverter[BigDecimal, Long] {
-
-    override def apply(o: BigDecimal): Long = o.toLong
-  }
-  implicit object BigDecimalToFloatConverter 
-    extends NumberConverter[BigDecimal, Float] {
-
-    override def apply(o: BigDecimal): Float = o.toFloat
-  }
-  implicit object BigDecimalToDoubleConverter 
-    extends NumberConverter[BigDecimal, Double] {
-
-    override def apply(o: BigDecimal): Double = o.toDouble
-  }
-  implicit object BigDecimalToBigIntConverter 
-    extends NumberConverter[BigDecimal, BigInt] {
-
-    override def apply(o: BigDecimal): BigInt = o.toBigInt()
-  }
-  implicit object BigDecimalToBigDecimalConverter 
-    extends NumberConverter[BigDecimal, BigDecimal] {
-
-    override def apply(o: BigDecimal): BigDecimal = o
+  implicit object NumberConverterIsInt extends NumberConverter[Int] {
+    override def fromInt(n: Int) = n
+    override def fromLong(n: Long) = n.toInt
+    override def fromFloat(n: Float) = n.toInt
+    override def fromDouble(n: Double) = n.toInt
+    override def toInt(n: Int): Int = n
+    override def toLong(n: Int): Long = n.toLong
+    override def toFloat(n: Int): Float = n.toFloat
+    override def toDouble(n: Int): Double = n.toDouble
+  }
+  implicit object NumberConverterIsLong extends NumberConverter[Long] {
+    override def fromInt(n: Int) = n.toLong
+    override def fromLong(n: Long) = n
+    override def fromFloat(n: Float) = n.toLong
+    override def fromDouble(n: Double) = n.toLong
+    override def toInt(n: Long): Int = n.toInt
+    override def toLong(n: Long): Long = n
+    override def toFloat(n: Long): Float = n.toFloat
+    override def toDouble(n: Long): Double = n.toDouble
+  }
+  implicit object NumberConverterIsFloat extends NumberConverter[Float] {
+    override def fromInt(n: Int) = n.toFloat
+    override def fromLong(n: Long) = n.toFloat
+    override def fromFloat(n: Float) = n
+    override def fromDouble(n: Double) = n.toFloat
+    override def toInt(n: Float): Int = n.toInt
+    override def toLong(n: Float): Long = n.toLong
+    override def toFloat(n: Float): Float = n
+    override def toDouble(n: Float): Double = n.toDouble
+  }
+  implicit object NumberConverterIsDouble extends NumberConverter[Double] {
+    override def fromInt(n: Int) = n.toDouble
+    override def fromLong(n: Long) = n.toDouble
+    override def fromFloat(n: Float) = n.toDouble
+    override def fromDouble(n: Double) = n
+    override def toInt(n: Double): Int = n.toInt
+    override def toLong(n: Double): Long = n.toLong
+    override def toFloat(n: Double): Float = n.toFloat
+    override def toDouble(n: Double): Double = n
   }
 }
-
